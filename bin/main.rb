90226efa2817ec9203c04d002d2b832103534f97
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-09-18 07:44:42 -0300

    Fixed styles rubocop errors / Global variables still present

--- bin/main.rb
+++ bin/main.rb
@@ -1,25 +1,25 @@
-# rubocop:disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity, Metrics/ModuleLength
+# rubocop:disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 
-#!/usr/bin/env ruby 
+# !/usr/bin/env ruby
 
 puts 'Welcome to our Tic-Tac-Toe Game!'
 
 puts 'Player 1, please type your beautiful name?'
-$player_one_name = gets.chomp
-puts "Nice name #{$player_one_name}!"
+player_one_name = gets.chomp
+puts "Nice name #{player_one_name}!"
 puts 'Please, notice that, from now on, you are going to be represented by the "X" characters on the board, right?'
 gets.chomp
 
 puts 'Player 2, how could we call this beautiful face?'
-$player_two_name = gets.chomp
-puts "Huuummmm #{$player_two_name}, I was pretty aware that your name would be also beautiful!"
-puts "As #{$player_one_name} is already using the 'X' character, you are going to be the 'O' character, nice?"
+player_two_name = gets.chomp
+puts "Huuummmm #{player_two_name}, I was pretty aware that your name would be also beautiful!"
+puts "As #{player_one_name} is already using the 'X' character, you are going to be the 'O' character, nice?"
 gets.chomp
 
-puts "#{$player_one_name} and #{$player_two_name}, please promise me you won't forget your characters?"
+puts "#{player_one_name} and #{player_two_name}, please promise me you won't forget your characters?"
 gets.chomp
 
-puts "Ok #{$player_one_name} and #{$player_two_name}, lets to the nuts and bolts!"
+puts "Ok #{player_one_name} and #{player_two_name}, lets to the nuts and bolts!"
 puts 'Here is the board of your game, with the respective number for each position, ok?'
 $plays = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -34,9 +34,8 @@ def catch_play
   play = play.to_i
   while $plays[play - 1] == 'X' || $plays[play - 1] == 'O' || !(play.to_i.positive? && play.to_i <= 9)
     play = gets.chomp.to_i
-    puts "play1 = #{play}"
     if $plays[play - 1] == 'X' || $plays[play - 1] == 'O' || !(play.to_i.positive? && play.to_i <= 9)
-      puts 'Ops, it seems you did not type a number between 1-9 OR someone already choosen this slot before :/' 
+      puts 'Ops, it seems you did not type a number between 1-9 OR someone already choosen this slot before :/'
     end
   end
   play
@@ -46,7 +45,7 @@ call_for_play_phrases = []
 call_for_play_phrases << 'So now, PLAYER, could you please choose one of the slots in the board?'
 call_for_play_phrases << 'PLAYER, now it\'s your turn!'
 call_for_play_phrases << 'PLAYER, no more. Choose your slot please.'
-call_for_play_phrases << 'PLAYER, please choose your slot and don\'t let your oponent\'s plan to conquer the world be sucessful!'
+call_for_play_phrases << 'PLAYER, choose your slot and don\'t let your oponent to conquer the world!'
 call_for_play_phrases << 'PLAYER, breath and think twice your next slot, it can be not so easy as you think!'
 call_for_play_phrases << 'PLAYER, what are you waiting for choose your slot??'
 call_for_play_phrases << 'PLAYER, please choose your slot, we don\'t have all the time of the world!!'
@@ -68,16 +67,15 @@ comment_play_phrases << 'Wow!!! Now I see your potential!'
 9.times do |i|
   char = 'X'
   if i.even?
-    $player = $player_one_name.dup
+    player = player_one_name.dup
   else
-    $player = $player_two_name.dup
+    player = player_two_name.dup
     char = 'O'
   end
-  puts call_for_play_phrases[rand(call_for_play_phrases.size - 1)].gsub('$PLAYER', $player)
+  puts call_for_play_phrases[rand(call_for_play_phrases.size - 1)].gsub('PLAYER', player)
   play = catch_play
-  puts "play = #{play}"
   $plays[play - 1] = char
   puts print_board($plays)
 end
 
-# rubocop:enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity, Metrics/ModuleLength
+# rubocop:enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
