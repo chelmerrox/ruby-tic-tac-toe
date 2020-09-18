c11477028c2cf26e57b62191bb7afe4fb09adab6
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-09-18 06:36:51 -0300

    Working for handle possible invalid inputs from user

--- bin/main.rb
+++ bin/main.rb
@@ -3,21 +3,21 @@
 puts 'Welcome to our Tic-Tac-Toe Game!'
 
 puts 'Player 1, please type your beautiful name?'
-player_one_name = gets.chomp
-puts "Nice name #{player_one_name}!"
+$player_one_name = gets.chomp
+puts "Nice name #{$player_one_name}!"
 puts 'Please, notice that, from now on, you are going to be represented by the "X" characters on the board, right?'
 gets.chomp
 
 puts 'Player 2, how could we call this beautiful face?'
-player_two_name = gets.chomp
-puts "Huuummmm #{player_two_name}, I was pretty aware that your name would be also beautiful!"
-puts "As #{player_one_name} is already using the 'X' character, and we don't to give you the 'Y', you are going to be the 'O' character, nice?"
+$player_two_name = gets.chomp
+puts "Huuummmm #{$player_two_name}, I was pretty aware that your name would be also beautiful!"
+puts "As #{$player_one_name} is already using the 'X' character, and we don't to give you the 'Y', you are going to be the 'O' character, nice?"
 gets.chomp
 
-puts "#{player_one_name} and #{player_two_name}, please promise me you won't forget your characters?"
+puts "#{$player_one_name} and #{$player_two_name}, please promise me you won't forget your characters?"
 gets.chomp
 
-puts "Ok #{player_one_name} and #{player_two_name}, lets to the nuts and bolts!"
+puts "Ok #{$player_one_name} and #{$player_two_name}, lets to the nuts and bolts!"
 puts 'Here is the board of your game, with the respective number for each position, ok?'
 $plays = [1,2,3,4,5,6,7,8,9]
 
@@ -32,30 +32,31 @@ play = ''
 def catch_play
   play = ''
   while play == ''
-    play = gets.chomp.to_i
-    puts "1 - plays[play - 1] = #{$plays[play - 1]}"
+    play = gets.chomp
+    puts "Hey #{$player}, it seems you did not type a number between 1-9" unless play.to_i > 0 && play.to_i <= 9
+    play = play.to_i
+    puts "play = #{play}"
     if $plays[play - 1] == 'X' || $plays[play - 1] == 'O'
       play = ''
       puts 'Ops, it seems someone already choosen this slot before :/'
     end
-    puts "Hey #{player}, it seems you did not type a number between 1-9" unless play.is_a?(Integer) || play > 9
   end
   play
 end
 
-call_for_play_phrases = ["So now, PLAYER, could you please choose one of the slots in the board?","PLAYER, now it's your turn!","PLAYER, no more. Choose your slot please.", "PLAYER, please choose your slot and don't let your oponent's plan to conquer the world be sucessful!", "PLAYER, breath and think twice your next slot, it can be not so easy as you think!"]
+call_for_play_phrases = ["So now, $PLAYER, could you please choose one of the slots in the board?","$PLAYER, now it's your turn!","$PLAYER, no more. Choose your slot please.", "$PLAYER, please choose your slot and don't let your oponent's plan to conquer the world be sucessful!", "$PLAYER, breath and think twice your next slot, it can be not so easy as you think!"]
 
 comment_play_phrases = ["Huummm... wise fox play!","WoW! I'm noticing this will be a giant's game!","Huummm!","ok",'Hey, how many years did you study this game?',"Couldn't you think something better than this??","Well I don't like this play, but anyway...","You never played this before, right?","... so amateur","Wow!!! Now I see your potential!"]
 
 9.times do |i|
   char = 'X'
   if i.even?
-    player = player_one_name.dup 
+    $player = $player_one_name.dup 
   else
-    player = player_two_name.dup 
+    $player = $player_two_name.dup 
     char = 'O'
   end
-  puts call_for_play_phrases[rand(call_for_play_phrases.size - 1)].gsub('PLAYER',player)
+  puts call_for_play_phrases[rand(call_for_play_phrases.size - 1)].gsub('$PLAYER',$player)
   play = catch_play
   puts "play = #{play}"
   $plays[play - 1] = char
