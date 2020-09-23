9022b006cc802c0679ab63d68054dd2ecedea6dc
Losalini <C.Rokocakau@gmail.com>
2020-09-24 06:45:15 +1200

    Define winning_board(boards_for_win=[]) method ro avoid repeated code

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,20 @@
 # rubocop:disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
+def winning_board(boards_for_win=[])
+    boards_for_win << [1, 2, 3]
+    boards_for_win << [1, 4, 7]
+    boards_for_win << [1, 5, 9]
+    boards_for_win << [2, 5, 8]
+    boards_for_win << [3, 5, 7]
+    boards_for_win << [3, 6, 9]
+    boards_for_win << [4, 5, 6]
+    boards_for_win << [7, 8, 9]
+end
+
 class Game
     def initialize; end
-  
+
     def check_winner?(plays, boards_for_win = [])
-      if boards_for_win == []
-        boards_for_win = [[1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 5, 7], [3, 6, 9], [4, 5, 6], [7, 8, 9]]
-      end
+      winning_board(boards_for_win)  if boards_for_win == []
   
       win = false
       boards_for_win.each do |board|
@@ -17,9 +26,7 @@ class Game
     end
   
     def check_draw?(plays, boards_for_win = [])
-      if boards_for_win == []
-        boards_for_win = [[1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 5, 7], [3, 6, 9], [4, 5, 6], [7, 8, 9]]
-      end
+      winning_board(boards_for_win)  if boards_for_win == []
   
       draw = true
   
