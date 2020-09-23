663956b3c198e1580066795064432d12818d514d
Losalini <C.Rokocakau@gmail.com>
2020-09-23 23:13:56 +1200

    Add class definittions that was removed from main.rb file

--- lib/classes.rb
+++ lib/classes.rb
@@ -7,6 +7,27 @@ class Player
   end
 end
 
+class Gameboard
+  def print_board(plays)
+    puts " #{plays[0]} | #{plays[1]} | #{plays[2]}"
+    puts " #{plays[3]} | #{plays[4]} | #{plays[5]}"
+    puts " #{plays[6]} | #{plays[7]} | #{plays[8]}"
+  end
+end
+
+class Gameplay
+  def catch_play(plays)
+    play = play.to_i
+    while plays[play - 1] == 'X' || plays[play - 1] == 'O' || !(play.to_i.positive? && play.to_i <= 9)
+      play = gets.chomp.to_i
+      if plays[play - 1] == 'X' || plays[play - 1] == 'O' || !(play.to_i.positive? && play.to_i <= 9)
+        puts 'Ops, it seems you did not type a number between 1-9 OR someone already choosen this slot before :/'
+      end
+    end
+    play
+  end
+end
+
 class Game
   def initialize; end
 
