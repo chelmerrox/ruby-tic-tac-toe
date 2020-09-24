a8508303e4c82afe3b96601f9f020d713d0713ca
Losalini <C.Rokocakau@gmail.com>
2020-09-24 20:01:01 +1200

    Delete gameboard.rb file and the Gameboard class in it

--- lib/gameboard.rb
+++
@@ -1,7 +0,0 @@
-class Gameboard
-  def print_board(plays)
-    puts " #{plays[0]} | #{plays[1]} | #{plays[2]}"
-    puts " #{plays[3]} | #{plays[4]} | #{plays[5]}"
-    puts " #{plays[6]} | #{plays[7]} | #{plays[8]}"
-  end
-end
