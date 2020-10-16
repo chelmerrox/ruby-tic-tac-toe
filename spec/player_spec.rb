05d539f82e2b46ef5acdad456f41be39fd89478b
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-10-16 09:21:04 -0300

    spec create for Player-rb

---
+++ spec/player_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require './lib/player.rb'
+
+describe Player do
+  let(:new_player) { Player.new }
+  describe '#additional_method' do
+    it "1 expected to return 1" do
+      expect(new_player.additional_method).to eql(1)
+    end
+  end
+end
