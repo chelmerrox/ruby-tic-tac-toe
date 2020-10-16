7fe84acee9b48813f69edeecb9b588df6f1ded67
Gilbert Gotora <gilbertgotora2@gmail.com>
2020-10-16 14:48:14 +0200

    edit README.md

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,7 +5,7 @@ require './lib/player.rb'
 describe Player do
   let(:new_player) { Player.new }
   describe '#additional_method' do
-    it "1 expected to return 1" do
+    it '1 expected to return 1' do
       expect(new_player.additional_method).to eql(1)
     end
   end
