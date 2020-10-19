d074b957878181c8f42070d3fbc1e8881a5df269
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-10-19 05:02:12 -0300

    Implement changes requested by 2nd TSE

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require './lib/player.rb'
+require_relative './lib/player.rb'
 
 describe Player do
-  let(:new_player) { Player.new }
+  let(:new_player) { Player.new('John', 'X') }
   describe '#additional_method' do
-    it "1 expected to return 1" do
+    it '1 expected to return 1' do
       expect(new_player.additional_method).to eql(1)
     end
   end
