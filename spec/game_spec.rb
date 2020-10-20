8e8cc772671c343296afac8fb4061288117683e6
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-10-20 04:50:08 -0300

    rspec added to 'winning_board' game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,11 @@ require_relative '../lib/game.rb'
 
 describe Game do
   let(:game_win_1) { Game.new }
+  describe '#winning_board' do
+    it '[] expected to return [[1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 5, 7], [3, 6, 9], [4, 5, 6], [7, 8, 9]]' do
+      expect(game_win_1.winning_board([])).to eql([[1, 2, 3], [1, 4, 7], [1, 5, 9], [2, 5, 8], [3, 5, 7], [3, 6, 9], [4, 5, 6], [7, 8, 9]])
+    end
+  end
   describe '#check_winner' do
     it "['X', 'X', 'X', 3, 4, 5, 6, 7, 8] expected to return true" do
       expect(game_win_1.check_winner?(['X', 'X', 'X', 3, 4, 5, 6, 7, 8])).to eql(true)
